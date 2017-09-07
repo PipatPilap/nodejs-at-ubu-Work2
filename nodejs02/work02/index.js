f447bb38161e87cd09dc0c7157a3581e91978d0f
Kewaleede <kewalee.de.57@ubu.ac.th>
2017-09-07 09:50:32 +0700

    Work021

--- nodejs02/work02/index.js
+++ nodejs02/work02/index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World! Pipat Pilap');
-    res.end('<h1 style="color:red;">Hello World! From Kewalee</h1>');
+    res.write('<h1 style="color:white;background-color:blue;">Hello World! Pipat Pilap<h1>'+'\n');
+    res.write('<h2 style="color:red;background-color:black;">Hello World! From Kewalee</h2>');
+    res.end();
 }).listen(8080);
