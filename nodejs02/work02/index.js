a3a04be88fda20b87b772ec50ef171c72c84b701
Pipat <pipat.max@gmail.com>
2017-09-07 09:03:20 +0700

    work02

---
+++ nodejs02/work02/index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World! Pipat Pilap');
+}).listen(8080);
